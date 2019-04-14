d58c6eb8561d4a97ffc1e7a661f748221430dcb1
Unknown <josselin.pennors@hotmail.fr>
2019-04-14 18:38:50 +0200

    Comments on module util

--- modules/utils.py
+++ modules/utils.py
@@ -19,9 +19,13 @@ def get_shape_stats(shapes, display_info=False):
 # Intersection area functions
 # A box is like this [ x, y, h, l ]
 def area(box):
+	"""Compute are of a box like this [x, y, h, l]"""
 	return box[2] * box[3]
 
+
 def area_between(box_1, box_2):
+	"""Compute overlap between two boxes like this [x, y, h, l]"""
+
 	# s: start, e: end
 	xs_1, ys_1, h1, l1 = box_1
 	xs_2, ys_2, h2, l2 = box_2
@@ -37,7 +41,9 @@ def area_between(box_1, box_2):
 	ye = min(ye_1, ye_2)
 	return (xe - xs) * (ye - ys)
 
+
 def area_rate(box_1, box_2):
+	"""Compute are rate between two boxes like this [x,y,h, l]"""
 	a_in = area_between(box_1, box_2)
 	if a_in == 0:
 		return 0
