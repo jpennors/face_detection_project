b76baabb689d272ac68783de1fd2f102387726a1
Unknown <josselin.pennors@hotmail.fr>
2019-04-15 00:29:30 +0200

    Bug in negative set

--- modules/negative_set.py
+++ modules/negative_set.py
@@ -15,16 +15,18 @@ def generate_box(img, box_ratio):
 	"""Generate a random box (x, y, h, l)"""
 	img_h, img_l = img.shape[:2]
 
-	# h = box_h + randint(-)
-
 	if int(img_h / box_ratio) < img_l:
-		h = randint(1, img_h - 1)
+		h = randint(int(40*box_ratio)+1, img_h - 1)
 	else :
-		h = randint(1, int(img_l*box_ratio) - 1)
+		h = randint(int(40*box_ratio)+1, int(img_l*box_ratio) - 1)
 
 	l = int(h/box_ratio)
-	x = randrange(img_l - l)
-	y = randrange(img_h - h)
+	x = randint(0, img_h - h)
+	y = randint(0, img_l - l)
+
+	if img_h < x+h or img_l < y+l:
+		print(img_l - l, img_h - h)
+		print(img_l, img_h, y+h, x+l)
 	
 	return x, y, h, l
 
