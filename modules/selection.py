bd8350e032d77f6e69a81e0b3423f261d96172a3
Alex <abrasseur.pro@gmail.com>
2019-04-16 23:56:23 +0200

    Add selection module for multiple tests

---
+++ modules/selection.py
@@ -0,0 +1,105 @@
+from . import models
+
+def try_params(images, label_sets, clf_name, global_params, changing_params):
+	"""
+	@brief      Multiple testing using changing and global parameters on one classifier
+	
+	@param      images           The images
+	@param      label_sets       The label sets (training and validation)
+	@param      clf_name         The classifier name
+	@param      global_params    The global parameters used for the classifier and the vectorization
+	@param      changing_params  The changing parameters used for the classifier and the vectorization
+	
+	@return     The results of the multiple tests
+	"""
+	assert len(label_sets) == 2, "Please provide training and validation labels"
+	train_labels, valid_labels = label_sets
+
+	global_vectorization_params = global_params.pop('vectorization_params')
+	global_box_size = global_params.pop('box_size')
+	param_results = []
+
+	# Try each changing parameter
+	for param_name in changing_params:
+		print(f"# Trying parameter `{param_name}`...")
+		for param_value in changing_params[param_name]:
+			print(f"    with value `{param_value}`")
+
+			# Build parameters with one changing
+			if param_name == 'vectorization_params':
+				box_size = global_box_size
+				model_params = global_params
+				vectorization_params = param_value
+			elif param_name == 'box_size':
+				box_size = param_value
+				model_params = global_params
+				vectorization_params = param_value				
+			else:
+				box_size = global_box_size
+				model_params = {
+					**global_params,
+					param_name: param_value,
+				}
+				vectorization_params = global_vectorization_params
+
+			# Build, train and validate classifier
+			clf = models.create_model(clf_name, model_params)
+			models.train(clf, images, box_size, train_labels, **vectorization_params)
+			score = models.predict_and_validate(clf, images, box_size, valid_labels, **vectorization_params)
+
+			# Add score to array
+			param_results.append({
+				'name': param_name,
+				'value': param_value,
+				'score': score,
+				'face_accuracy': score['true_pos'] / (score['true_pos'] + score['false_neg'] ),
+			})
+
+	return {
+		'classifier': clf_name,
+		'global_params': global_params,
+		'results': param_results,
+	}
+
+def try_classifiers(images, label_sets, global_params, changing_params={}):
+	"""
+	@brief      Multiple testing using changing and global parameters on multiple classifiers
+	
+	@param      images           The images
+	@param      label_sets       The label sets (training and validation)
+	@param      global_params    The global parameters used for each classifier and the vectorization
+															 with the classifier name as key
+	@param      changing_params  The changing parameters used for the classifier and the vectorization
+															 with the classifier name as key
+	
+	@return     The results of the multiple tests
+	"""
+	assert len(label_sets) == 2, "Please provide training and validation labels"
+	train_labels, valid_labels = label_sets
+
+	results = {}
+	for clf_name in global_params:
+		g_params = global_params[clf_name]
+		c_params = changing_params.get(clf_name)
+
+		if c_params:
+			results[clf_name] = try_params(images, label_sets, clf_name, g_params, c_params)
+		else:
+			box_size = g_params['box_size']
+			vectorization_params = g_params['vectorization_params']
+			model_params = { key: value for key, value in g_params.items()
+											 if key not in ('box_size', 'vectorization_params')}
+
+			clf = models.create_model(clf_name, model_params)
+			models.train(clf, images, box_size, train_labels, **vectorization_params)
+			score = models.predict_and_validate(clf, images, box_size, valid_labels, **vectorization_params)
+			results[clf_name] = {
+				'classifier': clf_name,
+				'global_params': global_params,
+				'results': [{
+					'score': score,
+					'face_accuracy': score['true_pos'] / (score['true_pos'] + score['false_neg'] ),
+				}],
+			}
+
+	return results
