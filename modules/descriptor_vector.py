89bcafe7d9e52bac460a789e609aff4c7552286b
Alex <abrasseur.pro@gmail.com>
2019-04-16 23:57:42 +0200

    Add some desciptors

--- modules/descriptor_vector.py
+++ modules/descriptor_vector.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage import feature
+from skimage.color import rgb2gray
 
 # Transform a [[n_images, h, l, 3]] shaped array with h, l constant
 # into a [[n_images, k]] shaped array where k is a constant
@@ -7,3 +8,9 @@ from skimage import feature
 def hog(images):
 	features = [ feature.hog(img, block_norm='L2') for img in images ]
 	return np.array(features)
+
+def flatten(images):
+	return np.array([ img.flatten() for img in images ])
+
+def gray_flatten(images):
+	return np.array([ rgb2gray(img).flatten() for img in images ])
